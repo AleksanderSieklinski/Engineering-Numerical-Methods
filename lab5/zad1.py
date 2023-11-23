f9d3e5d237febdedf50a9c50c6e1110c9786593b
Alex <aleksandersieklinski@gmail.com>
2023-11-23 10:43:02 +0100

    fixed lab5 completely

--- lab5/zad1.py
+++ lab5/zad1.py
@@ -12,31 +12,24 @@ TOL = 1e-8
 kArray = [16,8,4,2,1]
 
 def relaxate(V,k):
-	if k == 1:
-		for i in range(k,nx-k,k):
-			for j in range(k,ny-k,k):
-				V[i][j] = 0.25*(V[i-k][j]+V[i+k][j]+V[i][j-k]+V[i][j+k])
-	else:
-		for i in range(k,nx,k):
-			for j in range(k,ny,k):
-				try:
-					V[i][j] = 0.25*(V[i-k][j]+V[i+k][j]+V[i][j-k]+V[i][j+k])
-				except IndexError:
-					V[i][j] = 0.25*(V[i-k][j]+V[i+k-1][j]+V[i][j-k]+V[i][j+k-1])
+    for i in range(k,nx,k):
+        for j in range(k,ny,k):
+            V[i][j] = 0.25*(V[i+k][j] + V[i-k][j] + V[i][j+k] + V[i][j-k])
 
 def S(V,k):
     sum = 0
-    for i in range(0,nx-k,k):
-        for j in range(0,ny-k,k):
-            sum += (((k*delta)**2)/2)*( 
-            (((V[i+k][j]-V[i][j])/(2*k*delta)) + ((V[i+k][j+k]-V[i][j+k])/(2*k*delta)) )**2 + 
-            (((V[i][j+k]-V[i][j])/(2*k*delta)) + ((V[i+k][j+k]-V[i+k][j])/(2*k*delta)) )**2 )
+    for i in range(0,nx,k):
+        for j in range(0,ny,k):
+            sum += 0.5 * pow(k*delta,2)*(
+            pow((V[i+k,j] - V[i,j] + V[i+k,j+k] - V[i,j+k])/(2*k*delta),2)+
+            pow((V[i,j+k] - V[i,j] + V[i+k,j+k] - V[i+k,j])/(2*k*delta),2) 
+            )
     return sum
 
 def dens_mesh(V,k):
     k2 = k//2
-    for i in range(0,nx-k,k):
-        for j in range(k,ny-k,k):
+    for i in range(0,nx,k):
+        for j in range(0,ny,k):
             V[i+k2][j+k2] = 0.25*(V[i][j]+V[i+k][j]+V[i][j+k]+V[i+k][j+k])
             V[i+k][j+k2] = 0.5*(V[i+k][j]+V[i+k][j+k])
             V[i+k2][j+k] = 0.5*(V[i][j+k]+V[i+k][j+k])
@@ -44,35 +37,34 @@ def dens_mesh(V,k):
             V[i][j+k2] = 0.5*(V[i][j]+V[i][j+k])
 
 def main():
-    V = np.zeros((nx, ny))
-    S_values = []
+    V = np.zeros((nx+1, ny+1))
     sArray = {}
     iterArray = {}
-    # warunki brzegowe
-    for i in range(0, nx):
-        V[0][i] = math.sin(math.pi*(i*delta/ymax)) #Vb1
-        V[ny-1][i] = math.sin(math.pi*(i*delta/ymax)) #Vb3
-    for j in range(0, ny):
-        V[j][ny-1] = -math.sin(2*math.pi*(j*delta/xmax)) #Vb2
-        V[j][0] = math.sin(2*math.pi*(j*delta/xmax)) #Vb4
     # zad2 mapy potencjału dla wszystkich k
+    iter = 0
     for k in kArray:
+        # warunki brzegowe
+        for i in range(0, nx):
+            V[i][ny] = -math.sin(2*math.pi*(i*delta/xmax)) #Vb2
+            V[i][0] = math.sin(2*math.pi*(i*delta/xmax)) #Vb4
+        for j in range(0, ny):
+            V[0][j] = math.sin(math.pi*(j*delta/ymax)) #Vb1
+            V[nx][j] = math.sin(math.pi*(j*delta/ymax)) #Vb3
         print("k = ", k)
         sArr = []
-        iterArr = []
-        iter = 0
+        iterarr = []
+        s_prev = 1
         while True:
-            iterArr.append(iter)
             iter += 1
-            V_old = V.copy()
+            iterarr.append(iter)
             relaxate(V, k)
             S_value = S(V, k)
-            S_values.append(S_value)
             sArr.append(S_value)
-            if np.abs(S_value - S(V_old, k)) < TOL:
+            if np.abs((S_value - s_prev)/s_prev) < TOL:
                 break
+            s_prev = S_value
         sArray[k] = sArr
-        iterArray[k] = iterArr
+        iterArray[k] = iterarr
         plt.xlabel('x')
         plt.ylabel('y')
         plt.title('k='+str(k))
@@ -84,7 +76,7 @@ def main():
             dens_mesh(V, k)
     # zad2 zmiany S(it) dla wszystkich k
     for k in kArray:
-        plt.plot(iterArray[k],sArray[k], label='k='+str(k))
+        plt.plot(iterArray[k], sArray[k], label='k='+str(k))
     plt.title("S(it)")
     plt.xlabel('it')
     plt.ylabel('S')
