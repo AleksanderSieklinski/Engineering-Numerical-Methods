2bf401fa95d2cb038c5a1dd22fc0cb08e8fc78f0
Alex <aleksandersieklinski@gmail.com>
2023-11-26 22:25:05 +0100

    changed ticks on colormaps

--- lab6/plots.py
+++ lab6/plots.py
@@ -14,7 +14,7 @@ y_max = 50
 plt.xticks(np.arange(0, x_max/10 + 1, 5))
 plt.yticks(np.arange(0, y_max/10 + 1, 5))
 plt.imshow(arr, cmap='seismic',vmin=-10,vmax=10, extent=[0, x_max/10, 0, y_max/10])
-plt.colorbar()
+plt.colorbar(ticks=[-10,0,10])
 plt.savefig('z5_nx_ny_50_plot.png')
 # zad 5 podpunkt b
 arr = []
@@ -30,7 +30,7 @@ y_max = 100
 plt.xticks(np.arange(0, x_max/10 + 1, 5))
 plt.yticks(np.arange(0, y_max/10 + 1, 5))
 plt.imshow(arr, cmap='seismic',vmin=-10,vmax=10, extent=[0, x_max/10, 0, y_max/10])
-plt.colorbar()
+plt.colorbar(ticks=[-10,0,10])
 plt.savefig('z5_nx_ny_100_plot.png')
 # zad 5 podpunkt c
 arr = []
@@ -46,7 +46,7 @@ y_max = 200
 plt.xticks(np.arange(0, x_max/10 + 1, 5))
 plt.yticks(np.arange(0, y_max/10 + 1, 5))
 plt.imshow(arr, cmap='seismic',vmin=-10,vmax=10, extent=[0, x_max/10, 0, y_max/10])
-plt.colorbar()
+plt.colorbar(ticks=[-10,0,10])
 plt.savefig('z5_nx_ny_200_plot.png')
 # zad 6 podpunkt a
 arr = []
@@ -62,7 +62,7 @@ y_max = 100
 plt.xticks(np.arange(0, x_max/10 + 1, 5))
 plt.yticks(np.arange(0, y_max/10 + 1, 5))
 plt.imshow(arr, cmap='seismic',vmin=-1,vmax=1, extent=[0, x_max/10, 0, y_max/10])
-plt.colorbar()
+plt.colorbar(ticks=[-0.8,0,0.8])
 plt.savefig('z6_e1_1_e2_1_plot.png')
 # zad 6 podpunkt b
 arr = []
@@ -78,7 +78,7 @@ y_max = 100
 plt.xticks(np.arange(0, x_max/10 + 1, 5))
 plt.yticks(np.arange(0, y_max/10 + 1, 5))
 plt.imshow(arr, cmap='seismic',vmin=-1,vmax=1, extent=[0, x_max/10, 0, y_max/10])
-plt.colorbar()
+plt.colorbar(ticks=[-0.8,0,0.8])
 plt.savefig('z6_e1_1_e2_2_plot.png')
 # zad 6 podpunkt c
 arr = []
@@ -94,5 +94,5 @@ y_max = 100
 plt.xticks(np.arange(0, x_max/10 + 1, 5))
 plt.yticks(np.arange(0, y_max/10 + 1, 5))
 plt.imshow(arr, cmap='seismic',vmin=-1,vmax=1, extent=[0, x_max/10, 0, y_max/10])
-plt.colorbar()
+plt.colorbar(ticks=[-0.8,0,0.8])
 plt.savefig('z6_e1_1_e2_10_plot.png')
